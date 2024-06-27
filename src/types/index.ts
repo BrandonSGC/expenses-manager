78625d123f5a810ce66f811e26477b5de1aa6b9b
BrandonSGC <brangmz20@gmail.com>
2024-06-27 11:14:18 -0600

    Types

---
+++ src/types/index.ts
@@ -0,0 +1,19 @@
+export type Expense = {
+  id: string;
+  amount: number;
+  name: string;
+  category: string;
+  date: Value;
+};
+
+export type DraftExpense = Omit<Expense, "id">;
+
+export type ValuePiece = Date | null;
+export type Value = ValuePiece | [ValuePiece, ValuePiece];
+
+
+export type Category = {
+  id: string;
+  name: string;
+  icon: string;
+}
